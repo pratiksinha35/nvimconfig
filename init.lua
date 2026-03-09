024d7f9f96e584f30075a110cb529f232493defb
pratiksinha35 <pratiksinha35@gmail.com>
2026-03-09 10:58:29 -0400

    treesitter whichkey gitsigns guessindent

--- init.lua
+++ init.lua
@@ -2,3 +2,19 @@ require("core.options")
 require("core.keymaps")
 require("core.diagnostics")
 require("core.autocommands")
+
+-- ~/.config/nvim/init.lua
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
