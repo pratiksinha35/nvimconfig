111f5879c139ebc0bf892148c44f61b751095ff9
pratiksinha35 <pratiksinha35@gmail.com>
2026-03-10 12:13:11 -0400

    added ui elements

--- init.lua
+++ init.lua
@@ -1,20 +1,20 @@
-require("core.options")
-require("core.keymaps")
-require("core.diagnostics")
-require("core.autocommands")
+require 'core.options'
+require 'core.keymaps'
+require 'core.diagnostics'
+require 'core.autocommands'
 
 -- ~/.config/nvim/init.lua
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
     lazypath,
-  })
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require('lazy').setup 'plugins'
